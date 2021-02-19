42f261583493f960c793e280709424b57914eb4b
jkaok <jkaok@gmail.com>
2021-02-19 15:03:03 +0900

    2020.09

--- html/js/portfolio_index.js
+++ html/js/portfolio_index.js
@@ -3,22 +3,40 @@ $(function(){
     /* slide 바탕이미지 */
     var current = 0;
     
-    $(".slidelist").children("div:gt(0)").fadeOut();
-    $(".word").children("div:gt(0)").fadeOut();
+    // $(".slidelist").children("div:gt(0)").fadeOut();
+    // $(".word").children("div:gt(0)").fadeOut();
     
-    setInterval(function(){
-        var next = (current + 1) % 3; /* 1 - 2 - 3 - 1...*/
+    // setInterval(function(){
+    //     var next = (current + 1) % 3; /* 1 - 2 - 3 - 1...*/
         
-        $(".slidelist").find("div").eq(current).fadeOut(1000);
-        $(".slidelist").find("div").eq(next).fadeIn(1200);
+    //     $(".slidelist").find("div").eq(current).fadeOut(200);
+    //     $(".slidelist").find("div").eq(next).fadeIn(800);
         
-        $(".word").find("div").eq(current).fadeOut(1000);
-        $(".word").find("div").eq(next).delay(1200).fadeIn(1200);
+    //     // $(".word").find("div").eq(current).fadeOut();
+    //     // $(".word").find("div").eq(next).delay(600).fadeIn(500);
         
-        current = next;
+    //     current = next;
                 
-    }, 4500);
+    // }, 3000);
+
+
+   /*  Initialize Swiper */
+    var swiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
+        spaceBetween: 30,
+        effect: 'fade',
+        autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+        },
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        },
+        
+    });
     
+
 
     /* side 메뉴보이기 */
     $(".bars").click(function(){
