e9d7bb9cfd03ef7c5b81dabd340232330ff733d0
jkaok <jkaok@gmail.com>
2021-02-22 10:19:40 +0900

    2020.09

--- html/js/portfolio_parallax4.js
+++ html/js/portfolio_parallax4.js
@@ -8,37 +8,37 @@ $(function(){
     $(window).scroll(function(){
         var wScroll = $(this).scrollTop();
         
-        if( wScroll >= $cont2.offset().top - $(window).height()/3 ){
+        if( wScroll >= $cont2.offset().top - $(window).height()/2 ){
             $img.eq(0).addClass("show");
         }
-        if( wScroll >= $img.eq(1).offset().top - $(window).height()/3 ){
+        if( wScroll >= $img.eq(1).offset().top - $(window).height()/2 ){
             $img.eq(1).addClass("show");
         }
-        if( wScroll >= $img.eq(2).offset().top - $(window).height()/3 ){
+        if( wScroll >= $img.eq(2).offset().top - $(window).height()/2 ){
             $img.eq(2).addClass("show");
         }
-        if( wScroll >= $img.eq(3).offset().top - $(window).height()/3 ){
+        if( wScroll >= $img.eq(3).offset().top - $(window).height()/2 ){
             $img.eq(3).addClass("show");
         }
-        if( wScroll >= $img.eq(4).offset().top - $(window).height()/3 ){
+        if( wScroll >= $img.eq(4).offset().top - $(window).height()/2 ){
             $img.eq(4).addClass("show");
         }
-        if( wScroll >= $img.eq(5).offset().top - $(window).height()/3 ){
+        if( wScroll >= $img.eq(5).offset().top - $(window).height()/2 ){
             $img.eq(5).addClass("show");
         }
-        if( wScroll >= $img.eq(6).offset().top - $(window).height()/3 ){
+        if( wScroll >= $img.eq(6).offset().top - $(window).height()/2 ){
             $img.eq(6).addClass("show");
         }
-        if( wScroll >= $img.eq(7).offset().top - $(window).height()/3 ){
+        if( wScroll >= $img.eq(7).offset().top - $(window).height()/2 ){
             $img.eq(7).addClass("show");
         }
-        if( wScroll >= $img.eq(8).offset().top - $(window).height()/3 ){
+        if( wScroll >= $img.eq(8).offset().top - $(window).height()/2 ){
             $img.eq(8).addClass("show");
         }
-        if( wScroll >= $img.eq(9).offset().top - $(window).height()/3 ){
+        if( wScroll >= $img.eq(9).offset().top - $(window).height()/2 ){
             $img.eq(9).addClass("show");
         }
-        if( wScroll >= $img.eq(10).offset().top - $(window).height()/3 ){
+        if( wScroll >= $img.eq(10).offset().top - $(window).height()/2 ){
             $img.eq(10).addClass("show");
         }
         if( wScroll >= $img.eq(11).offset().top - $(window).height()/2 ){
